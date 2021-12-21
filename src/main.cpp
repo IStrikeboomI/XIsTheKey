fc86928f4f42e55224b4a988b7d91c94e4baa74a
Shubh Makadia <60586926+IStrikeboomI@users.noreply.github.com>
2021-12-21 16:22:53 -0600

    added comment to main

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,5 @@
+//Whenever we press the X key, play a vine thud and show an X fade in and out
+
 #include "hook/KeyboardHook.h"
 int main() {
 	//start off by initalizing the keyboard hook for incoming key presses
